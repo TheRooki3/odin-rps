178c7fa95c793b66a3fe81079bf39eb7df3b568a
TheRookie <therookiecode@gmail.com>
2024-04-23 11:58:09 -0600

    Added: Game Checker to compare the computer choice to yours

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+
+
 
 const getComputerChoice = () =>{
 	const randomNum = Math.round(Math.random() * 2) + 1;
@@ -17,3 +20,38 @@ const getComputerChoice = () =>{
 };
 
 console.log(getComputerChoice());
+
+const getHumanChoice = () =>{
+	let choice = prompt("Enter Rock, Paper, or Scissors").toLowerCase();
+	console.log(choice);
+	if (choice === "rock" || choice === "paper" || choice === "scissors"){
+		gameCheck(choice,getComputerChoice());
+	}else{
+		alert("Invalid Input");
+		choice = prompt("Enter Rock, Paper, or Scissors").toLowerCase();
+	}
+};
+
+const gameCheck = (playerChoice, computerChoice) =>{
+	if(playerChoice === "rock" && computerChoice === "scissors"){
+		alert(`The Computer Chose ${computerChoice}`);
+		alert("You Win!");
+		playerScore ++;
+	}else if(playerChoice === "paper" && computerChoice === "rock"){
+		alert(`The Computer Chose ${computerChoice}`);
+		alert("You Win!");
+		playerScore ++;
+	}else if(playerChoice === "scissors" && computerChoice === "paper"){
+		alert(`The Computer Chose ${computerChoice}`);
+		alert("You Win!");
+		playerScore ++;
+	}else if( playerChoice == computerChoice){
+		alert(`The Computer Chose ${computerChoice}`);
+		alert("Tie!");
+	}else{
+		alert(`The Computer Chose ${computerChoice}`);
+		alert("You Lose!");
+	}
+};
+
+getHumanChoice();
